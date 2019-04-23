003747ea63fe1d12b526f0fc49edc282323fa3b8
Jonathan Flowers <jmflowers134@gmail.com>
2019-04-23 10:47:04 -0400

    Update views.py

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,5 +1,12 @@
 from products.models import Product
 from api.serializers import ProductSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
 
-# YOUR VIEWS HERE
+
+class ProductAPIView(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all() 
