aa0d6a82bd17baee572f36f17c39e0487c158c81
Gindi Khangura <khangura_9@hotmail.com>
2019-07-27 17:32:06 -0500

    Implemented classes. Shamelessly used solution branch for help.

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -3,6 +3,17 @@ from rest_framework import serializers
 from products.models import Product, Category
 
 
-class ProductSerializer():
+class ProductSerializer(serializers.ModelSerializer):
     # YOUR CODE HERE
-    pass
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'sku',
+            'category',
+            'description',
+            'price',
+            'created',
+            'featured',
+        )
