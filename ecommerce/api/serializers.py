f8e34b875efda3a89027ed8458678708463f9652
manofoar <joetynan@gmail.com>
2019-09-09 02:47:24 +0000

    bugfixes

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -3,9 +3,9 @@ from rest_framework import serializers
 from products.models import Product, Category
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
         fields = ('id', 'name', 'sku', 'category', 'description', 'price',
                   'created', 'featured',)
-    
